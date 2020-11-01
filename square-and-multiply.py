47a2864854a133e35fc6b5b4ed819d66bcbebc61
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2020-11-01 14:20:23 -0500

    Fix -h option

--- square-and-multiply.py
+++ square-and-multiply.py
@@ -1,5 +1,6 @@
 import argparse
 
+
 def square_and_multiply(base, power, modulus):
     result = base
     iter_binary = iter('{0:b}'.format(power))
@@ -14,9 +15,9 @@ def square_and_multiply(base, power, modulus):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("base", help="the base of this equation", type=int)
-    parser.add_argument("exponent", help="the exponent of this equations", type=int)
-    parser.add_argument("modulus", help="the modulus to calculate base^exponent % modulus", type=int)
+    parser.add_argument("base", help="the base to convert to binary", type=int)
+    parser.add_argument("exponent", help="the exponent", type=int)
+    parser.add_argument("modulus", help="the modulus", type=int)
     args = parser.parse_args()
 
     print("{0}^{1} mod {2} = {3}".format(args.base, args.exponent, args.modulus, square_and_multiply(args.base, args.exponent, args.modulus)))
