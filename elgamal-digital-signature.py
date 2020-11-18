c6c5c43f909d554256cc0eaf5a5fcd5073f95eec
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2020-11-18 18:13:51 -0500

    Add description

--- elgamal-digital-signature.py
+++ elgamal-digital-signature.py
@@ -19,12 +19,12 @@ def verify_signature(r, s, p, a, b, x, show_steps):
     ax = pow(a, x, p)
     if show_steps:
         print(f"\nt = β^r * r^s mod p:\n\t{t} = {b}^{r} * {r}^{s} mod {p}")
-        print(f"α^x mod p:\n\t{ax} = {a}^{x} mod {p}")
+        print(f"\nα^x mod p:\n\t{ax} = {a}^{x} mod {p}")
     print(f"\nVerification (t, α^x) = ({t}, {ax})\n")
     return t == ax
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Walks through the Elgamal Digital Signature process and checks if the signature is valid.")
     parser.add_argument("d", help="the private key, a random integer d ∈ {2, 3, ..., p - 2}", type=int)
     parser.add_argument("p", help="large prime p", type=int)
     parser.add_argument("α", help="primitive root α", type=int)
@@ -37,12 +37,12 @@ def main():
     b = pow(a, d, p)
     if args.show_steps:
         print(f"Choose d = {d}\nChoose p = {p}\nChoose α = {a}\n")
-        print(f"Compute β: \nβ = α^d mod p:\n\t{b} = {a}^{d} mod {p}\n")
-    print(f"Public Key (p, α, β) = ({p}, {a}, {b})")
+        print(f"β = α^d mod p:\n\t{b} = {a}^{d} mod {p}\n")
+    print(f"Public Key (p, α, β) = ({p}, {a}, {b})\n")
 
     x, ke = args.x, args.kE
     if args.show_steps:
-        print(f"\nChoose x = {x}")
+        print(f"Choose x = {x}")
         print(f"Choose kE = {ke}")
 
     r, s = generate_signature(d, p, a, x, ke, args.show_steps)
