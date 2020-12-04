7c47a017fa4b3f1d41bfa4a01d875cfe6dfc0e60
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2020-12-03 20:42:17 -0500

    Change param names

--- scripts/caesar-shift-cipher.py
+++ scripts/caesar-shift-cipher.py
@@ -5,17 +5,17 @@ from prettytable import PrettyTable
 def main():
     parser = argparse.ArgumentParser(description="Implements basic ASCII encryption/decryption through a Ceasar cipher, as well as brute forcing.")
     parser.add_argument("text", help="the plaintext or ciphertext to encrypt or decrypt respectively", type=str)
-    parser.add_argument("-s", "--shift", help="the shift (if known, no value = brute force)", type=int)
+    parser.add_argument("-k", "--key", help="the shift (if known, no value = brute force)", type=int)
     args = parser.parse_args()
 
     ascii_start = ord('A') if args.text.isupper() else ord('a')
-    if not args.shift:
+    if not args.key:
         table = PrettyTable()
         table.field_names = ["Shift", "Text"]
         [table.add_row([i, ''.join(chr((ord(c) + i - ascii_start) % 26 + ascii_start) for c in args.text)]) for i in range(26)]
         print(table)
     else:
-        print(f"Shifted: {''.join(chr((ord(c) + args.shift - ascii_start) % 26 + ascii_start) for c in args.text)}")
+        print(f"Shifted: {''.join(chr((ord(c) + args.key - ascii_start) % 26 + ascii_start) for c in args.text)}")
 
 
 if __name__ == "__main__":
